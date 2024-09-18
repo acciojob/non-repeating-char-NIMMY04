044b54f37f37b91012de837aaf9bf73749369472
NIMMY04 <156294603+NIMMY04@users.noreply.github.com>
2024-09-18 17:12:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ let n= str.length();
+	for(i=0;i<n;i++){
+		let found= true;
+		for(j=i+1;j<n;j++){
+			if(s[i]===s[j])
+				found == false;
+			break;
+		}
+		if(found){
+			return i;
+		}
+	}
+	return -1;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
